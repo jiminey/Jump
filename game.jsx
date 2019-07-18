fe885f8f1b0bd0288f9838a8db9818348199f3cd
jiminey <jtwnguyen@ucdavis.edu>
2019-07-18 09:52:13 -0700

    Create, center, and color canvas

---
+++ game.jsx
@@ -0,0 +1,30 @@
+const cvs = document.getElementById("game");
+const ctx = cvs.getContext('2d');
+
+
+
+let frames = 0; 
+
+
+
+
+
+
+function draw() {
+    ctx.fillStyle = "#70c5ce";
+    ctx.fillRect(0, 0, cvs.width, cvs.height)
+}
+
+function update() {
+
+}
+
+
+function loop() {
+    update();
+    draw();
+    frames++;
+    requestAnimationFrame(loop); 
+}
+
+loop();
