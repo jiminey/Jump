1c203e5ef7fc71de6c7fbf1655852a1a82b4801f
jiminey <jtwnguyen@ucdavis.edu>
2019-07-18 14:28:36 -0700

    Implement acceleration for directions

--- game.js
+++ game.js
@@ -9,64 +9,120 @@ const sprite = new Image();
 sprite.src = "src/assets/tileset.png"
 
 
+let key_watcher = {
+    39 : false,
+    37 : false, 
+    38 : false 
+}
+
+
+document.addEventListener("keydown", function (evt) {
+ 
+    if (evt.keyCode in key_watcher) {
+        key_watcher[evt.keyCode] = true;
+
+        if (evt.keyCode === 39) {
+            chara.right = true;
+            chara.x += chara.xspeed;
+            
+        }
+        if (evt.keyCode === 37) {
+            chara.left = true;
+            chara.x += chara.xspeed;
+        }
+        if (evt.keyCode === 38) {
+            chara.hop();
+        }       
+    }  
+});
+
+document.addEventListener("keyup", function (evt) {
+    if (evt.keyCode in key_watcher) {
+        key_watcher[evt.keyCode] = false;
+    }
+});
+
+
 const fg = {
     sX : 18,
     sY : 96,
     w : 220,
     h : 48,
     x : 0,
-    y : cvs.height- 18, 
+    y : cvs.height - 48, 
 
     draw : function() {
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w+30, this.h - 30)
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w +30, this.y, this.w+30, this.h - 30)
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w + 48, this.h)
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w +48, this.y, this.w + 48, this.h)
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w + 48 + this.w + 48, this.y, this.w + 48, this.h)
+
     }
 }
 
-const char = {
+const chara = {
     sX: 175,
     sY: 150,
     w: 17,
     h: 25,
     x: 250,
-    y: cvs.height - 70, 
+    y: cvs.height - fg.h - 48, 
 
-
+    xspeed: 10,
+    velocity: 0.05, 
     frame: 0,
-    gravity: 0.25,
-    jump: 6.6,
-    speed: 0,
-    jump_counter: 0,
+    gravity: 0.05,
+    jump: 15,
+    yspeed: 0,
+    jump_counter: 2,
+    left: false,
+    right: false, 
 
     
-    jump : function () {
-        if (this.jump_counter > 0) {
-            this.speed = -this.jump;
-            this.y = this.y - 1;
+    hop : function () {
+        if (this.jump_counter !== 0) {
+            this.jump -= this.velocity; 
+
+            this.y -= 30;
             this.jump_counter -= 1;
         }
-        
+    
     },
     
     
     draw : function() {
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h)
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w * 2, this.h * 2 )
     },
 
     update : function() {
-        
-            this.speed += this.gravity;
+            if (this.left) {
+                this.left = !this.left; 
+                if (this.velocity > 0) {
+                    this.velocity = this.velocity * -1;
+                    this.xspeed = 10;
+                }
+            } else if (this.right) {
+                this.right = !this.right;
+                if (this.velocity < 0) {
+                    this.velocity = this.velocity * -1;
+                    this.xspeed = 10;
+                }
+            }
+
+            this.yspeed += this.gravity; //fall faster the higher you fall
+            this.xspeed += this.velocity;
+            
 
             //ground logic 
-            if (this.y >= cvs.height - fg.h) {
-                this.y = cvs.height - fg.h; //stays on ground 
-                this.speed = 0; 
+            if (this.y >= cvs.height - fg.h - 48) {
+                this.speed += this.velocity; 
+                this.y = cvs.height - fg.h - 48; //stays on ground 
+                this.yspeed = 0; 
                 this.jump_counter = 2; //reset jump counter
             }
 
             //air logic
             if (this.y < cvs.height - fg.h) {
-                this.y += this.speed; // this changes y position, gravity
+                this.y += this.yspeed; // this changes y position, gravity
             }
 
 
@@ -83,11 +139,11 @@ function draw() {
     ctx.fillRect(0, 0, cvs.width, cvs.height)
 
     fg.draw();
-    char.draw();
+    chara.draw();
 }
 
 function update() {
-    char.update();
+    chara.update();
 }
 
 
