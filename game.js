c70f5aa18ddf82fb08e2d0cb0e83ba5c168931ac
jiminey <jtwnguyen@ucdavis.edu>
2019-07-18 10:59:46 -0700

    Add jump logic

--- game.js
+++ game.js
@@ -15,14 +15,63 @@ const fg = {
     w : 220,
     h : 48,
     x : 0,
-    y : cvs.height - 48, 
+    y : cvs.height- 18, 
 
     draw : function() {
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w+60, this.h)
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w+60, this.h)
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w+30, this.h - 30)
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w +30, this.y, this.w+30, this.h - 30)
+    }
+}
+
+const char = {
+    sX: 175,
+    sY: 150,
+    w: 17,
+    h: 25,
+    x: 250,
+    y: cvs.height - 70, 
+
+
+    frame: 0,
+    gravity: 0.25,
+    jump: 6.6,
+    speed: 0,
+    jump_counter: 0,
+
+    
+    jump : function () {
+        if (this.jump_counter > 0) {
+            this.speed = -this.jump;
+            this.y = this.y - 1;
+            this.jump_counter -= 1;
+        }
+        
+    },
+    
+    
+    draw : function() {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h)
+    },
+
+    update : function() {
+        
+            this.speed += this.gravity;
+
+            //ground logic 
+            if (this.y >= cvs.height - fg.h) {
+                this.y = cvs.height - fg.h; //stays on ground 
+                this.speed = 0; 
+                this.jump_counter = 2; //reset jump counter
+            }
+
+            //air logic
+            if (this.y < cvs.height - fg.h) {
+                this.y += this.speed; // this changes y position, gravity
+            }
 
 
     }
+
 }
 
 
@@ -30,14 +79,15 @@ const fg = {
 
 
 function draw() {
-    ctx.fillStyle = "#70c5ce";
+    ctx.fillStyle = "#ccc";
     ctx.fillRect(0, 0, cvs.width, cvs.height)
 
     fg.draw();
+    char.draw();
 }
 
 function update() {
-
+    char.update();
 }
 
 
