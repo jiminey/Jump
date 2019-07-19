a7f74a0b67df07dc2c843f12d25421340c60f4bf
jiminey <jtwnguyen@ucdavis.edu>
2019-07-19 11:46:44 -0700

    Render platform

--- foreground.js
+++ foreground.js
@@ -4,25 +4,10 @@ import sprite from '../src/assets/tileset.png';
 class Foreground {
 
     constructor(canvas, context) {
-        this.cvs = canvas;
-        this.ctx = context;
-
-        this.sprite = new Image();
-        this.sprite.src = sprite;
-
-        this.sX = 18;
-        this.sY = 96;
-        this.w = 220;
-        this.h = 48;
-        this.x = 0;
-        this.y = this.cvs.height - 48;
+        
     }
 
-    draw() {
-        this.ctx.drawImage(this.sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w + 48, this.h);
-        this.ctx.drawImage(this.sprite, this.sX, this.sY, this.w, this.h, this.x + this.w + 48, this.y, this.w + 48, this.h);
-        this.ctx.drawImage(this.sprite, this.sX, this.sY, this.w, this.h, this.x + this.w + 48 + this.w + 48, this.y, this.w + 48, this.h);
-    }
+    
 
     update() {
 
--- game.js
+++ game.js
@@ -1,21 +1,16 @@
-import Player from './Player';
-import Foreground from './Foreground';
-
-
-
-const cvs = document.getAnimations("game");
+const cvs = document.getElementById("game");
 const ctx = cvs.getContext("2d");
 
 let frames = 0;
 let platforms = []; 
 
-let player = new Player(cvs, ctx, platforms); 
-let fg = new Foreground(cvx, ctx);
+sprite = new Image();
+sprite.src = "src/assets/tileset.png";
 
 document.addEventListener("keydown", keyDown);
 document.addEventListener("keyup", keyUp);
 
-for (i = 0; i < 10; i++) {
+for (i = 0; i < 30; i++) {
     platforms.push(
         {
             x: Math.random() * cvs.width,
@@ -26,12 +21,74 @@ for (i = 0; i < 10; i++) {
     );
 } //generate platforms
 
+const player = {
+    sX : 175,
+    sY : 150,
+    w : 17,
+    h : 25,
+    x : 200,
+    y : 200,
+
+    xvelocity : 0,
+    yvelocity : 0,
+    onGround : false,
+    holdLeft : false,
+    holdRight : false,
+    gravity : 0.5,
+
+    draw : function() {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w * 2, this.h * 2)
+    },
+
+    update : function() {
+        if (this.holdLeft) this.xvelocity = -2;
+        if (this.holdRight) this.xvelocity = 2;
+
+        this.x += this.xvelocity;
+        this.y += this.yvelocity;
+
+        if (this.onGround) {
+            this.xvelocity *= 0.78; //friction on ground
+        } else {
+            this.yvelocity += this.gravity; //falling speed
+        }
+
+        this.onGround = false;
+
+        for (let i = 0; i < 30; i++) {
+            if (this.x > platforms[i].x &&
+                
+                this.x < platforms[i].x + platforms[i].w &&
+                this.y > platforms[i].y &&
+                this.y < platforms[i].y + platforms[i].h
+            ) {
+                this.y = platforms[i].y;
+                this.onGround = true;
+            }
+        }
+    } 
+}
+
+const fg = {
+    sX : 18,
+    sY : 96,
+    w : 220,
+    h : 48,
+    x : 0,
+    y : cvs.height - 48,
+
+    draw() {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w + 48, this.h);
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w + 48, this.y, this.w + 48, this.h);
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w + 48 + this.w + 48, this.y, this.w + 48, this.h);
+    }
+}
 
 
 function keyDown(evt){
     switch(evt.keyCode){
         case 37:
-            player.holdLeft = true;
+            player.holdLeft = true
             break; 
         case 38:
             if (player.onGround) {
@@ -80,6 +137,7 @@ function draw() {
 
 
 function update() {
+    player.update();
     
 }
 
